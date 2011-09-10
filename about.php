532cacd4285f0997a48c86a0e0049ad3c0fa63b7
JJE Admin <jje@area51.heliohost.org>
2011-09-10 07:29:21 +0100

    Fixes #4

--- about.php
+++ about.php
@@ -13,7 +13,7 @@ require 'header.php';
 
 <table class="content" align="center"><tr><td>
 <p><b>HelioMobile<br>
-Version 0.1 BETA</b></p>
+Version 0.1.1 BETA</b></p>
 <p>HelioMobile is opensource software made of PHP, HTML and CSS. If you
 have found a bug, have a suggestion, or wish to help HelioMobile and
 use your own coding skills to contribute, please visit our <a href="https://github.com/HelioNetworks/HelioMobile">GitHub
